ca52c73adbcba87305d054587bdab131eeb6d88c
rocketandonuts <rocketandonuts@gmail.com>
2018-07-07 22:27:47 +0900

    HL_p047
    HL_p047

---
+++ HL_p047/playMusic.js
@@ -0,0 +1,13 @@
+//var bgm = new Audio("CaveWaltzinanutshell.mp3");
+//bgm.play();
+
+var bgm = new Audio("CaveWaltzinanutshell.mp3");
+
+function PlaySound()
+{
+    bgm.play();
+    ani();
+}
+
+
+
